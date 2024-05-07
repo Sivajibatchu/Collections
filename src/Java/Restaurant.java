22417c3aaa484a11bf077e05902418f2b6645439
lenovo <sivajibatchu8328@gmail.com>
2024-05-07 16:28:47 +0530

    Restaurant

---
+++ src/Java/Restaurant.java
@@ -0,0 +1,9 @@
+public class Restaurant {
+    public  String name;
+}
+
+public static void main(String[] args) {
+    Restaurant restaurant = new  Restaurant();
+    restaurant.name = "Pista House";
+    System.out.println(restaurant);
+}
